a4c4114ffe68b62f14134800e482f701df572a9c
Patrick Looft <tug16392@CST11211.tu.temple.edu>
2019-03-28 12:02:49 -0400

    Started adding ui, created player class

--- lab5/ViewController.swift
+++ lab5/ViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //top of screen
+    @IBOutlet weak var player1Score: UILabel!;
+    @IBOutlet weak var player1Name: UILabel!;
+    @IBOutlet weak var player2Score: UILabel!;
+    @IBOutlet weak var player2Name: UILabel!;
+    @IBOutlet weak var diceLabel: UILabel!;
+    
+    //buttons
+    @IBOutlet weak var holdBtn: UIButton!;
+    @IBOutlet weak var rollBtn: UIButton!;
+    @IBOutlet weak var newGameBtn: UIButton!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //greys out buttons upon app entry
+        holdBtn.isEnabled = false;
+        rollBtn.isEnabled = false;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
