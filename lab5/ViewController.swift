172b4e51cc1a4be47fc77ec7d1b521e1c9ee513f
helpdesk <helpdesk@helpme.ocis.temple.edu>
2019-03-28 21:57:35 -0400

    wrok commit, trying to link function to roll button, havent done any of them yet tho. They are linked to UIOutlets, might need to make it an IBAction instead

--- lab5/ViewController.swift
+++ lab5/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var holdBtn: UIButton!;
     @IBOutlet weak var rollBtn: UIButton!;
     @IBOutlet weak var newGameBtn: UIButton!;
+    @IBOutlet weak var p1ProgView: UIProgressView!;
+    @IBOutlet weak var p2ProgView: UIProgressView!;
+    
     
     //Players:
     var p1 = Player();
@@ -34,15 +37,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //pressed the new game button
     @IBAction func newGameStarted(sender: UIButton){
         //re-enable buttons since game started
+        print("MAde it into the function");
         holdBtn.isEnabled = true;
         rollBtn.isEnabled = true;
-        
-        //create two player objects
+        p1.setIsTurn(turns: true);
+        p2.setIsTurn(turns: false);
         
     }
     
+    //roll button pressed
     @IBAction func rollBtnPressed(sender: UIButton){
         //logic flow: press button, receive roll, check to see if that makes user win, add score
         if(p1.getIsTurn()){
@@ -52,6 +58,9 @@ class ViewController: UIViewController {
                 //print winner message
                 diceLabel.text = "PLAYER ONE WINS";
             }
+            //update progress view
+            p1ProgView.progress = (Float(p1.getScore())/100.00);
+            
         }
         else if(p2.getIsTurn()){
             let roll = p2.diceRoll();
@@ -60,12 +69,15 @@ class ViewController: UIViewController {
                 //print winner message
                 diceLabel.text = "PLAYER ONE WINS";
             }
+            
         }
         else{
             print("This case should never happen, it should always be someones turn. This shouldnt print");
         }
+        p2ProgView.progress = (Float(p1.getScore())/100.00);
     }
     
+    //hold button pressed
     @IBAction func holdBtnPressed(sender: UIButton){
         if(p1.getIsTurn()){//p1's turn and chooses to hold
             p2.setIsTurn(turns: true);
@@ -80,6 +92,7 @@ class ViewController: UIViewController {
         }
     }
     
+
 }
 
 
